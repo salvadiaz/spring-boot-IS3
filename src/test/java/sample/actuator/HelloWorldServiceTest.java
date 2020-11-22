8457521117b3e600b1fe74ed3607d0b58aa08dfb
Carolina <1700250@ucc.edu.ar>
2020-11-21 21:06:03 -0300

    Tp 9 test added

--- src/test/java/sample/actuator/HelloWorldServiceTest.java
+++ src/test/java/sample/actuator/HelloWorldServiceTest.java
@@ -1,6 +1,8 @@
 package sample.actuator;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -12,4 +14,18 @@ public class HelloWorldServiceTest {
 		assertEquals("Expected correct message","Spring boot says hello from a Docker container",helloWorldService.getHelloMessage());
 	}
 	
+	public void anotherTest() {
+		HelloWorldService mockFoo = mock(HelloWorldService.class);
+		
+		when(mockFoo.getHelloMessage())
+		.thenReturn("Hola Hola")
+		.thenReturn("Hello Hello");
+		
+		
+		assertEquals("Expected correct message","Hola Hola",mockFoo.getHelloMessage());
+		assertEquals("Expected correct message","Hello Hello",mockFoo.getHelloMessage());
+	}
 }
+
+
+
